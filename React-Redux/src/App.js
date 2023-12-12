68f1f3f5e701ff35205d1a43539c2a3e4705b74f
hussain <hussainabdullatif@marun.edu.tr>
2023-12-12 23:17:05 +0300

    move file to redux folder

--- React-Redux/src/App.js
+++ React-Redux/src/App.js
@@ -1,7 +1,7 @@
 import logo from "./logo.svg";
 import "./App.css";
 import { Provider } from "react-redux";
-import store from "./redux/cake/store";
+import store from "./redux/store";
 
 import CakeContainer from "./components/CakeContainer";
 
