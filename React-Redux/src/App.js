9674e4797a9c5caede8fa80be431fd7eac42f576
hussain <hussainabdullatif@marun.edu.tr>
2023-12-12 23:32:17 +0300

    Add hooks to redux
    - useSelector()
    - useDispatch()

--- React-Redux/src/App.js
+++ React-Redux/src/App.js
@@ -1,15 +1,19 @@
-import logo from "./logo.svg";
 import "./App.css";
 import { Provider } from "react-redux";
 import store from "./redux/store";
 
 import CakeContainer from "./components/CakeContainer";
+import HooksCakeContainer from "./components/HooksCakeContainer";
 
 function App() {
     return (
         <Provider store={store}>
             <div className="App">
+                <h3>Using Connect Function</h3>
                 <CakeContainer />
+                <hr />
+                <h3>Using Hooks</h3>
+                <HooksCakeContainer />
             </div>
         </Provider>
     );
